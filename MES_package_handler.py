fdefc3e43959111cf9764e243c1cde956d69d24c
BansheeLT <tweek693@gmail.com>
2022-09-25 19:20:11 +0700

    SBAT SINFO done

--- MES_package_handler.py
+++ MES_package_handler.py
@@ -1,4 +1,5 @@
 import base64
+import enum
 import json
 import struct
 import time
@@ -12,6 +13,21 @@ import time
 # Pbat
 # Sost ???
 # FwVer
+
+class device_error_code(enum.Enum):
+    no_error = "00"
+    no_respone = "01"
+    power_overload = "02"
+    low_voltage = "03"
+    thermometer_elctro_leak = "04"
+    thermometer_error_scan = "05"
+    thermometer_is_missing = "06"
+    error_1wire = "07"
+    calibration_data_error = "08"
+    thermosensor_is_missing = "09"
+    thermometer_is_too_long = "10"
+    queue_overflow = "20"
+    clock_chip_damaged = "30"
 
 class packet_factory(object):
     pass    
@@ -48,7 +64,6 @@ class inclinometer_data_packet(packet):
         [self.y_angle] = struct.unpack('f', bArr_y)
         [self.x_angle] = struct.unpack('f', bArr_x)
         self.timestamp = int.from_bytes(bArr_uts, 'big')
-        print('\tAng X: ' + '{0:.0f}'.format(self.x_angle))
     def __init__(self, rx_json):
         super().__init__(rx_json)
         self.x_angle = None
@@ -82,21 +97,57 @@ class hygrometer_data_packet(packet):
         super().__init__(rx_json)
 
 class battery_info_packet(packet):
-    def __init__(self, rx_json) -> None:
+    def __battery_info_decode(self, str_hex_data):
+        bArr_v_bat = bytearray()
+        for i in range(0, 4):
+            bArr_v_bat.append(str_hex_data[4 - i])
+        [self.u_battery] = struct.unpack('f', bArr_v_bat)
+        self.battery_level = round(((self.u_battery - 3.0)/(3.6 - 3.0))*100.0)
+        print("1") 
+    def __init__(self, rx_json):
         super().__init__(rx_json)
+        self.battery_level = None
+        self.u_battery = None
+        self.__battery_info_decode(self._hex_data)
     def __str__(self) -> str:
-        return super().__str__()
+        return (super().__str__() 
+                + f"\nu_battery: {self.u_battery}"
+                + f"\nbattery_level: {self.battery_level}" + "%")
 
 class status_packet_info(packet):
+    def __decode_status(self, str_hex_data):
+        self.error_code = str(str_hex_data[1])
+        self.protocol_version = str(str_hex_data[4])
+        self.firmmware_version = str(str_hex_data[5]) + "." + str(str_hex_data[6])
+        self.device_version = str(str_hex_data[7]) + str(str_hex_data[8])
     def __init__(self, rx_json):
         super().__init__(rx_json)
         self.error_code = None
+        self.protocol_version = None
+        self.firmmware_version = None
+        self.device_version = None
+        self.__decode_status(self._hex_data)
     def __str__(self):
-        return super().__str__()
+        return (super().__str__() 
+                + f"\nerror_code: {self.error_code}"
+                + f"\nprotocol_version: {self.protocol_version}"
+                + f"\nfirmware_version: {self.firmmware_version}"
+                + f"\ndevice_version: {self.device_version}")
 
 if __name__ == "__main__":
     print("Entry point: MES_package_handler")
-    inc_11_o = open("debug/Inclinometer_07293314052DFF9E/11.txt", 'r')
-    rx_json = json.load(inc_11_o)
-    p = inclinometer_data_packet(rx_json)
-    print(p.__str__())
+    # inc_11_o = open("debug/Inclinometer_07293314052DFF9E/11.txt", 'r')
+    # rx_json = json.load(inc_11_o)
+    # p = inclinometer_data_packet(rx_json)
+    # print(p.__str__())
+    
+    # inc_12_o = open("debug/Inclinometer_07293314052DFF9E/12.txt", 'r')
+    # rx_json = json.load(inc_12_o)
+    # sp = status_packet_info(rx_json)
+    # print(sp.__str__())
+
+
+    inc_13_o = open("debug/Thermometer_07293314052dff55/13.txt", 'r')
+    rx_json = json.load(inc_13_o)
+    p_bat = battery_info_packet(rx_json)
+    print(p_bat.__str__())
